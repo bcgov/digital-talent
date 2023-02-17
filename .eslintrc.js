b90c80b1e17204bca4857b75674dde4c6d62bd04
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2023-02-17 14:25:57 -0800

    initial www commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,27 @@ module.exports = easelint({
   jsxPragma: 'react',
   eslint: {
     root: true,
+    overrides: [
+      {
+        files: ['apps/www/**/*.{js,jsx,ts,tsx}'],
+        extends: ['plugin:@next/next/recommended'],
+        settings: {
+          'import/resolver': {
+            typescript: {
+              project: 'apps/www/tsconfig.json',
+            },
+          },
+          next: {
+            rootDir: 'apps/www',
+          },
+        },
+        rules: {
+          '@next/next/no-html-link-for-pages': ['error', 'apps/www/src/pages'],
+          'import/no-default-export': 'off',
+          'react/react-in-jsx-scope': 'off',
+        },
+      },
+    ],
     ignorePatterns: ['**/dist/**/*.js'],
   },
 });
