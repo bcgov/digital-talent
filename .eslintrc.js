6b10a8b882edf1816509874e408eede9b99add56
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2023-03-20 11:24:27 -0700

    initial hms api commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,14 @@ module.exports = easelint({
     root: true,
     overrides: [
       {
+        // Files which require devDependencies
+        //  config files, tests, etc...
+        files: ['*.e2e-spec.ts', '*.spec.ts'],
+        rules: {
+          'import/no-extraneous-dependencies': 'off',
+        },
+      },
+      {
         // apps/api | Strapi
         files: ['apps/api/**/*.{js,jsx,ts,tsx}'],
         rules: {
@@ -15,6 +23,21 @@ module.exports = easelint({
           '@typescript-eslint/no-explicit-any': 'warn',
           'import/no-default-export': 'off',
           'no-console': 'warn',
+        },
+      },
+      {
+        // apps/hms-api | NestJS
+        files: ['apps/hms-api/**/*.{js,jsx,ts,tsx}'],
+        env: {
+          node: true,
+          jest: true,
+        },
+        rules: {
+          '@typescript-eslint/interface-name-prefix': 'off',
+          '@typescript-eslint/explicit-function-return-type': 'off',
+          '@typescript-eslint/explicit-module-boundary-types': 'off',
+          '@typescript-eslint/no-explicit-any': 'off',
+          'class-methods-use-this': 'off',
         },
       },
       {
