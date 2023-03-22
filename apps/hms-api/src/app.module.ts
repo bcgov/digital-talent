5f331f1edab2cd16c35b88a77412b5f3e625aaf4
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2023-03-22 16:03:07 -0700

    add auth user, public, role decorators

--- apps/hms-api/src/app.module.ts
+++ apps/hms-api/src/app.module.ts
@@ -5,12 +5,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { AuthGuard } from './auth/guards/auth.guard';
-import { validate } from './validation/environment.validation';
+import { RoleGuard } from './auth/guards/role.guard';
 import { PrismaService } from './services/prisma/prisma.service';
+import { validate } from './validation/environment.validation';
 
 @Module({
   imports: [CacheModule.register({ isGlobal: true }), ConfigModule.forRoot({ isGlobal: true, validate }), AuthModule],
   controllers: [AppController],
-  providers: [{ provide: APP_GUARD, useClass: AuthGuard }, AppService, PrismaService],
+  providers: [
+    { provide: APP_GUARD, useClass: AuthGuard },
+    { provide: APP_GUARD, useClass: RoleGuard },
+    AppService,
+    PrismaService,
+  ],
 })
 export class AppModule {}
