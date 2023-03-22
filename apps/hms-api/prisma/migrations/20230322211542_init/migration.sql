c397fd0487b996e6a1348a2c4f09708b62f7899f
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2023-03-22 14:56:42 -0700

    add prisma, logic to sync users from keycloak tokens

--- apps/hms-api/prisma/migrations/20230322211542_init/migration.sql
+++ apps/hms-api/prisma/migrations/20230322211542_init/migration.sql
@@ -7,3 +7,6 @@ CREATE TABLE "User" (
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_idir_id_key" ON "User"("idir_id");
--- apps/hms-api/src/services/prisma/prisma.service.ts
+++ apps/hms-api/src/services/prisma/prisma.service.ts
@@ -13,3 +13,4 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     });
   }
 }
+// a
