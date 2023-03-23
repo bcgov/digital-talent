90abffa9b9e7cd08b341ad8928390d0266cc6170
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2023-03-23 06:42:06 -0700

    save user in localStorage rather than sessionStorage

--- apps/hms-www/src/main.tsx
+++ apps/hms-www/src/main.tsx
@@ -1,10 +1,14 @@
+import { WebStorageStateStore } from 'oidc-client-ts';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { AuthProvider } from 'react-oidc-context';
+import { AuthProvider, AuthProviderProps } from 'react-oidc-context';
 import App from './App';
 import './index.css';
 
-const oidcConfig = {
+const oidcConfig: AuthProviderProps = {
+  userStore: new WebStorageStateStore({
+    store: localStorage,
+  }),
   authority: import.meta.env.VITE_KEYCLOAK_REALM_URL,
   client_id: import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
   redirect_uri: import.meta.env.VITE_KEYCLOAK_REDIRECT_URL,
