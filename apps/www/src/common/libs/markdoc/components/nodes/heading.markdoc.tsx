c57fe434f9141b0ed64ed273c898a1b8b3e63c50
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2023-05-11 10:45:40 -0700

    set top margin on markdoc headers

--- apps/www/src/common/libs/markdoc/components/nodes/heading.markdoc.tsx
+++ apps/www/src/common/libs/markdoc/components/nodes/heading.markdoc.tsx
@@ -10,37 +10,37 @@ export default function Heading({ id, children, level }: any) {
   switch (level) {
     case 1:
       return (
-        <h1 className="text-3xl font-semibold" {...(id && { id })}>
+        <h1 className="mt-0 text-3xl font-semibold" {...(id && { id })}>
           {children}
         </h1>
       );
     case 2:
       return (
-        <h2 className="text-2xl font-semibold" {...(id && { id })}>
+        <h2 className="mt-0 text-2xl font-semibold" {...(id && { id })}>
           {children}
         </h2>
       );
     case 3:
       return (
-        <h3 className="text-xl font-semibold" {...(id && { id })}>
+        <h3 className="mt-0 text-xl font-semibold" {...(id && { id })}>
           {children}
         </h3>
       );
     case 4:
       return (
-        <h4 className="text-lg font-semibold" {...(id && { id })}>
+        <h4 className="mt-0 text-lg font-semibold" {...(id && { id })}>
           {children}
         </h4>
       );
     case 5:
       return (
-        <h5 className="text-base font-semibold" {...(id && { id })}>
+        <h5 className="mt-0 text-base font-semibold" {...(id && { id })}>
           {children}
         </h5>
       );
     case 6:
       return (
-        <h6 className="text-sm font-semibold" {...(id && { id })}>
+        <h6 className="mt-0 text-sm font-semibold" {...(id && { id })}>
           {children}
         </h6>
       );
