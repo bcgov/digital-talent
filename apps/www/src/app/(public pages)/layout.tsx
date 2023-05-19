26f4e5936277fe0085d63efe13a60b4456b94eee
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2023-05-19 08:57:05 -0700

    add initial landing page

--- apps/www/src/app/(public pages)/layout.tsx
+++ apps/www/src/app/(public pages)/layout.tsx
@@ -1,11 +1,11 @@
 import { PropsWithChildren } from 'react';
 // import Content from '../../../../common/components/public/layout/Content';
-import Footer from '../../../../common/components/public/layout/Footer';
-import Header from '../../../../common/components/public/layout/Header';
+import Footer from '../../common/components/public/layout/Footer';
+import Header from '../../common/components/public/layout/Header';
 
 export default function PublicLayout({ children }: PropsWithChildren) {
   return (
-    <div className="h-screen">
+    <div className="flex flex-col h-screen">
       <Header />
       <div className="grow pt-12">{children}</div>
       <Footer />
