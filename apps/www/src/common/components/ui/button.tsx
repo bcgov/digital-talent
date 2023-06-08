5725680257f22295c00860d711a8a536239050d3
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2023-06-08 11:22:54 -0700

    update button styles

--- apps/www/src/common/components/ui/button.tsx
+++ apps/www/src/common/components/ui/button.tsx
@@ -8,10 +8,10 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        'call-to-action': 'bg-bcgov-gold text-black',
+        default: 'bg-bcgov-blue-dark text-primary-foreground hover:bg-bcgov-blue-dark/90',
+        'call-to-action': 'bg-bcgov-gold text-black hover:bg-bcgov-gold/90',
         destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline: 'border border-input hover:bg-accent hover:text-accent-foreground',
+        outline: 'border border-bcgov-blue-dark text-bcgov-blue-dark hover:bg-accent hover:text-accent-foreground',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'underline-offset-4 hover:underline text-primary',
