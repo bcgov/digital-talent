d35ed512bb112e49cca226153849e48bc8a7a21a
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2023-06-29 07:34:22 -0700

    Add snowplow analytics

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from 'next';
 import { Montserrat, Noto_Sans_Display, Noto_Sans_Mono, Noto_Serif_Display } from 'next/font/google';
+import Analytics from '../common/components/analytics.component';
 import { wrapTitle } from '../common/utils/wrap-title.util';
 import './globals.css';
-
 
 const montserrat = Montserrat({ subsets: ['latin'], variable: '--font-montserrat', weight: 'variable' });
 const notoSans = Noto_Sans_Display({ subsets: ['latin'], variable: '--font-noto-sans-display', weight: 'variable' });
@@ -18,8 +18,6 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
- 
-
   return (
     // Note: smooth scrolling introduces a scroll bug where it doesn't scroll all the way to the top of the page
     // when navigating between internal links. This is addressed in header component by temporarily disabling smooth scroll when
@@ -28,6 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body
         className={`${montserrat.variable} ${notoSans.variable} ${notoSansMono.variable} ${notoSerif.variable} font-sans`}
       >
+        <Analytics />
         {children}
       </body>
     </html>
