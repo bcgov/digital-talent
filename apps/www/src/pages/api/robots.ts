7b874fe404fac05e043fcb402c0b9af51398747e
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2023-06-29 16:11:25 -0700

    update robots.txt to allow crawling in production

--- apps/www/src/pages/api/robots.ts
+++ apps/www/src/pages/api/robots.ts
@@ -1,7 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.send(`User-agent: *
+  res.send(
+    process.env.NODE_ENV === 'production'
+      ? `User-agent: *
+Allow: /
+`
+      : `User-agent: *
 Disallow: /
-`);
+`,
+  );
 }
