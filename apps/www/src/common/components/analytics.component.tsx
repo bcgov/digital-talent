4fe86d51ef1101a2caa65d39cdd17e95a28ce1c5
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2023-07-05 09:23:35 -0700

    Update snowplow collector

--- apps/www/src/common/components/analytics.component.tsx
+++ apps/www/src/common/components/analytics.component.tsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 
 export default function Analytics() {
   useEffect(() => {
-    newTracker('rt', 'spm.apps.gov.bc.ca', {
+    newTracker('rt', 'spt.apps.gov.bc.ca', {
       appId: 'Snowplow_standalone_OCIO',
       cookieLifetime: 86500 * 548,
       platform: 'web',
