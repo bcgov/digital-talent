ab995aae67b3a30dbb21182dce407bcbaa46bf01
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2023-09-07 09:07:30 -0700

    refactor skill module

--- apps/hms-api/src/modules/skill/skill.module.ts
+++ apps/hms-api/src/modules/skill/skill.module.ts
@@ -3,10 +3,11 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { PrismaModule } from '../prisma/prisma.module';
 import { SkillCommandHandlers } from './commands';
 import { SkillEventHandlers } from './events';
-import { SkillResolver } from './skill.resolver';
+import { SkillQueryHandlers } from './queries';
+import { SkillResolver } from './resolvers/skill.resolver';
 
 @Module({
   imports: [CqrsModule, PrismaModule],
-  providers: [SkillResolver, ...SkillCommandHandlers, ...SkillEventHandlers],
+  providers: [SkillResolver, ...SkillCommandHandlers, ...SkillEventHandlers, ...SkillQueryHandlers],
 })
 export class SkillModule {}
