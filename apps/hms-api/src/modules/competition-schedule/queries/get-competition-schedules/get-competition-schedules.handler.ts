29ee067ff65f16ed68068bf105198bd01155eed8
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2023-09-12 13:30:35 -0700

    add competition link to competition schedule resolver

--- apps/hms-api/src/modules/competition-schedule/queries/get-competition-schedules/get-competition-schedules.handler.ts
+++ apps/hms-api/src/modules/competition-schedule/queries/get-competition-schedules/get-competition-schedules.handler.ts
@@ -3,13 +3,14 @@ import { PrismaService } from '../../../prisma/prisma.service';
 import { GetCompetitionSchedulesQuery } from './get-competition-schedules.query';
 
 @QueryHandler(GetCompetitionSchedulesQuery)
-export class GetCompetitionSchedulesHandler implements IQueryHandler<GetCompetitionSchedulesHandler> {
+export class GetCompetitionSchedulesHandler implements IQueryHandler<GetCompetitionSchedulesQuery> {
   constructor(private readonly prisma: PrismaService) {}
 
-  async execute(query: GetCompetitionSchedulesHandler): Promise<any> {
+  async execute(query: GetCompetitionSchedulesQuery): Promise<any> {
     return this.prisma.competitionSchedule.findMany({
       where: {
         deleted_at: null,
+        ...(query.args?.where && { ...query.args.where }),
       },
     });
   }
--- apps/hms-api/src/modules/competition-schedule/queries/get-competition-schedules/get-competition-schedules.query.ts
+++ apps/hms-api/src/modules/competition-schedule/queries/get-competition-schedules/get-competition-schedules.query.ts
@@ -1,1 +1,9 @@
-export class GetCompetitionSchedulesQuery {}
+import { Prisma } from '@prisma/client';
+
+interface GetCompetitionSchedulesQueryArgs {
+  where?: Prisma.CompetitionScheduleWhereInput;
+}
+
+export class GetCompetitionSchedulesQuery {
+  constructor(readonly args?: GetCompetitionSchedulesQueryArgs) {}
+}
