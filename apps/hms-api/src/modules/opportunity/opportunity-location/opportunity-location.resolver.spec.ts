464fa89b6e406a3a9c627ee5c10ab2d8f01daead
Mandryk <Kaegan.Mandryk@gov.bc.ca>
2023-09-12 13:22:55 -0700

    opportunity-location and opportunity tests

--- apps/hms-api/src/modules/opportunity/opportunity-location/opportunity-location.resolver.spec.ts
+++ apps/hms-api/src/modules/opportunity/opportunity-location/opportunity-location.resolver.spec.ts
@@ -193,4 +193,59 @@ describe('OpportunityLocationResolver', () => {
       }),
     );
   });
+
+  it('should create an opportunityLocation correctly', async () => {
+    const input: CreateOpportunityLocationInput = {
+      opportunity_id,
+      location_id,
+    };
+
+    const input2: CreateOpportunityLocationInput = {
+      opportunity_id: opportunity_id2,
+      location_id: location_id2,
+    };
+
+    await resolver.createOpportunityLocation({ id: userId } as any, input);
+
+    await resolver.createOpportunityLocation({ id: userId } as any, input2);
+
+    expect(mockCommandBus.execute).toHaveBeenCalledWith(
+      expect.objectContaining({
+        ...new CreateOpportunityLocationCommand({ ...input }, { created_by: userId }),
+      }),
+    );
+  });
+
+  it('should get all opportunityLocations correctly', async () => {
+    const mockOpportunityLocations = [{ id: '1' }, { id: '2' }];
+    mockPrismaService.opportunityLocation.findMany.mockResolvedValueOnce(mockOpportunityLocations);
+
+    const result = await resolver.opportunityLocations();
+    // eslint-disable-next-line no-console
+    console.log(result);
+    expect(result).toEqual(mockOpportunityLocations);
+    expect(mockPrismaService.opportunityLocation.findMany).toHaveBeenCalled();
+  });
+
+  it('should get a specific opportunityLocation by id correctly', async () => {
+    const mockOpportunityLocation = { opportunity_id, location_id };
+    mockPrismaService.opportunityLocation.findUnique.mockResolvedValueOnce(mockOpportunityLocation);
+
+    const result = await resolver.opportunityLocation(opportunity_id, location_id);
+
+    expect(result).toEqual(mockOpportunityLocation);
+    expect(mockPrismaService.opportunityLocation.findUnique).toHaveBeenCalledWith({
+      where: { opportunity_id_location_id: mockOpportunityLocation },
+    });
+  });
+
+  it('should delete an opportunityLocation correctly', async () => {
+    await resolver.deleteOpportunityLocation({ id: userId } as any, opportunity_id, location_id);
+
+    expect(mockCommandBus.execute).toHaveBeenCalledWith(
+      expect.objectContaining({
+        ...new DeleteOpportunityLocationCommand({ opportunity_id, location_id }, { created_by: userId }),
+      }),
+    );
+  });
 });
