2f046ea9ac453ad6fc234b584e721a07f6c942b2
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2023-09-12 13:30:35 -0700

    add competition link to competition schedule resolver

--- apps/hms-api/src/modules/competition-schedule/resolvers/competition-schedule.resolver.ts
+++ apps/hms-api/src/modules/competition-schedule/resolvers/competition-schedule.resolver.ts
@@ -1,7 +1,9 @@
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
 import { GraphQLString } from 'graphql';
 import { CurrentUser } from '../../auth/decorators/current-user.decorator';
+import { CompetitionModel } from '../../competition/competition/models/competition.model';
+import { GetCompetitionQuery } from '../../competition/competition/queries/get-competition/get-competition.query';
 import { CreateCompetitionScheduleCommand } from '../commands/create-competition-schedule/create-competition-schedule.command';
 import { DeleteCompetitionScheduleCommand } from '../commands/delete-competition-schedule/delete-competition-schedule.command';
 import { UpdateCompetitionScheduleCommand } from '../commands/update-competition-schedule/update-competition-schedule.command';
@@ -50,15 +52,16 @@ export class CompetitionScheduleResolver {
 
   @Query((returns) => [CompetitionScheduleModel])
   async competitionSchedules() {
-    // return this.prisma.competitionSchedule.findMany();
     return this.queryBus.execute(new GetCompetitionSchedulesQuery());
   }
 
   @Query((returns) => CompetitionScheduleModel)
   async competitionSchedule(@Args('id', { type: () => GraphQLString }) id: string) {
-    // return this.prisma.competitionSchedule.findUnique({
-    //   where: { id },
-    // });
     return this.queryBus.execute(new GetCompetitionScheduleQuery(id));
+  }
+
+  @ResolveField('competition', (returns) => CompetitionModel)
+  async getCompetition(@Parent() competitionSchedule: CompetitionScheduleModel) {
+    return this.queryBus.execute(new GetCompetitionQuery(competitionSchedule.competition_id));
   }
 }
